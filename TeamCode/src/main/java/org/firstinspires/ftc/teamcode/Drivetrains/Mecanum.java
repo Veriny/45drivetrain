f7e4474200b9e97ee195bdb8988c13faf6274d85
Justin Im <20imj68@myahs.org>
2018-09-09 17:11:05 -0700

    Started Mecanum, was too lazy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrains/Mecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrains/Mecanum.java
@@ -1,8 +1,33 @@
 package org.firstinspires.ftc.teamcode.Drivetrains;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
 /**
  * Created by your mom on 9/9/18.
  */
 
 public class Mecanum {
+    public DcMotor rightTop;
+    public DcMotor rightBottom;
+    public DcMotor leftTop;
+    public DcMotor leftBottom;
+    public Mecanum(DcMotor TR, DcMotor BR, DcMotor TL, DcMotor BL){
+        this.rightBottom = BR;
+        this.leftBottom = BL;
+        this.rightTop = TR;
+        this.leftTop = TL;
+        leftTop.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftBottom.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+    public void rightDrive(Double power){
+
+    }
+    public void leftDrive(Double power){
+
+    }
+    public void arcadeDrive(Double power){
+
+    }
+
 }
